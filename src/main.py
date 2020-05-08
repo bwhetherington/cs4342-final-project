4ac77581edd5e1444f338f848ee600f2f65f49b4
Ben Hetherington <b.w.hetherington@gmail.com>
2020-05-07 23:14:53 -0400

    Use keras

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ import numpy as np
 import neural
 import sys
 
+import keras
+from keras import layers
+
 
 NUM_HANDS = 10
 
@@ -13,6 +16,7 @@ def prepare_y(hands):
     for i, hand in zip(np.arange(n), hands):
         y[i][hand] = 1
     return y
+    # return hands
 
 
 card_size = 4 + 13
@@ -86,20 +90,69 @@ def split_data(X, Y):
     return X_train, Y_train, X_test, Y_test
 
 
+def augment(X, Y):
+    resamples = 10
+    n, f = X.shape
+
+    # We will include 3 shuffled versions of each hand for each hand in the input data
+    X_aug = np.zeros((n * resamples, f))
+    Y_aug = np.zeros((n * resamples, 10))
+
+    for i, x, y in zip(np.arange(n), X, Y):
+        start = i * resamples
+        Y_aug[start:start + resamples] = y
+        for j in range(resamples):
+            x_aug = np.copy(x).reshape((5, 17))
+            np.random.shuffle(x_aug)
+            x_aug = x_aug.flatten()
+            X_aug[start + j] = x_aug
+
+    return X_aug, Y_aug
+
+
 def main():
     X, Y = load_data('train.csv')
+    X, Y = augment(X, Y)
     X_train, Y_train, X_test, Y_test = split_data(X, Y)
 
-    nn = neural.NeuralNetwork(hidden_neurons=70)
-    nn.train(X_train, Y_train, X_test=X_test,
-             Y_test=Y_test, batch_size=40, learning_rate=0.1, num_epochs=75, learning_rate_decay=False)
+    model = keras.Sequential([
+        layers.Dense(70, name='hidden1', activation='relu', input_shape=(85,)),
+        layers.Dense(60, name='hidden2', activation='relu'),
+        layers.Dense(30, name='hidden3', activation='relu'),
+        layers.Dense(10, name='output', activation='softmax')
+    ], 'poker_predictor')
 
-    X_submit = load_test('test.csv')
-    np.save("X_submit.npy", X_submit)
+    model.summary()
 
-    # X_submit = np.load("X_submit.npy")
+    model.compile(
+        optimizer=keras.optimizers.RMSprop(),  # Optimizer
+        # Loss function to minimize
+        loss=keras.losses.CategoricalCrossentropy(),
+        # List of metrics to monitor
+        metrics=["categorical_accuracy"],
+    )
 
-    predictions = nn.decision_function(X_submit).argmax(axis=0)
+    model.fit(
+        x=X_train,
+        y=Y_train,
+        batch_size=40,
+        epochs=50,
+        validation_data=(X_test, Y_test)
+    )
+    model.save_weights('model.h5')
+
+    # model.load_weights('model.h5')
+
+    # nn = neural.NeuralNetwork(hidden_neurons=70)
+    # nn.train(X_train, Y_train, X_test=X_test,
+    #          Y_test=Y_test, batch_size=40, learning_rate=0.1, num_epochs=75, learning_rate_decay=False)
+
+    # X_submit = load_test('test.csv')
+    # np.save("X_submit.npy", X_submit)
+
+    X_submit = np.load("X_submit.npy")
+
+    predictions = np.array(model.predict(X_submit)).argmax(axis=1)
     indices = np.arange(len(predictions)) + 1
     data = np.vstack((indices, predictions)).T
     d = pd.DataFrame(data=data, columns=["id", "hand"])
